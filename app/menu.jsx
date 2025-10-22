058a244cad76d57752856ff59d92b337e7aa701a
ZakiaTaou <zakiataou99@gmail.com>
2025-10-22 15:18:16 +0100

    feat(menu): display coffee items list with images using FlatList

--- app/menu.jsx
+++ app/menu.jsx
@@ -1,19 +1,72 @@
 import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+  Image,
+} from "react-native";
 import { useRouter } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 
 export default function menu() {
   const router = useRouter();
+  const menuItems = [
+    {
+      id: 1,
+      name: "Espresso",
+      price: "10 MAD",
+      image: require("../assets/images/Espresso.jpg"),
+    },
+    {
+      id: 2,
+      name: "Cappuccino",
+      price: "18 MAD",
+      image: require("../assets/images/Cappuccino.jpg"),
+    },
+    {
+      id: 3,
+      name: "Latte",
+      price: "16 MAD",
+      image: require("../assets/images/Latte.jpg"),
+    },
+    {
+      id: 4,
+      name: "Cold Brew",
+      price: "20 MAD",
+      image: require("../assets/images/ColdBrew.jpg"),
+    },
+    {
+      id: 5,
+      name: "Mocha",
+      price: "20 MAD",
+      image: require("../assets/images/Mocha.jpg"),
+    },
+  ];
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <TouchableOpacity onPress={() => router.back()}>
-          <Ionicons name="arrow-back" size={30} color="#3B2F2F" />
+          <Ionicons
+            style={styles.headerIcon}
+            name="arrow-back"
+            size={30}
+            color="#3B2F2F"
+          />
         </TouchableOpacity>
         <Text style={styles.headerTitle}>Menu</Text>
       </View>
+      <FlatList
+        data={menuItems}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Image source={item.image} style={styles.image} />
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -28,12 +81,28 @@ const styles = StyleSheet.create({
   header: {
     flexDirection: "row",
     alignItems: "center",
-    margin:30,
-    gap:25,
+    margin: 30,
+    gap: 25,
   },
   headerTitle: {
     fontSize: 35,
     fontWeight: "bold",
     color: "#3B2F2F",
-  }
+  },
+  headerIcon: {
+    cursor: "pointer",
+  },
+  card: {
+    backgroundColor: "#F2E6D8",
+    borderRadius: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 15,
+    padding: 12,
+  },
+  image: {
+    width: 80,
+    height: 80,
+    borderRadius: 35,
+  },
 });
