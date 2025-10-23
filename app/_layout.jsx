4ea4cd73384f5f77db23df17e26582d5e42ad282
reghanan06-hue <reghanan06@gmail.com>
2025-10-23 11:11:07 +0100

    update home screen

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -5,7 +5,6 @@ export default function RootLayout() {
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
       <Stack.Screen name="menu" />
-      <Stack.Screen name="order" />
 
     </Stack>
   );
