4db12ad2e74812a9c1201cf1347e73b1212ff26c
reghanan06-hue <reghanan06@gmail.com>
2025-10-23 09:46:26 +0100

    update app

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,12 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return<Stack   screenOptions={{headerShown:false}}>
-    <Stack.Screen name="index"/>
-  </Stack>;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="menu" />
+      <Stack.Screen name="order" />
+
+    </Stack>
+  );
 }
